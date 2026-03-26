8f871c9941a803258c67ebe9bbeb07c5b20f1956
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2026-03-26 09:09:31 +0700

    fix:adding image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,59 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false, 
+              },
+            },
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        quality: 80,
+      },
+      avif: {
+        // https://sharp.pixelplumbing.com/api-output#avif
+        quality: 70,
+      },
+    }),
+  ],
   base: '/aimisy-ou/',
 })
